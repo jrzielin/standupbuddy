56a45683f3c25dd2f7152e98e44925d50ca981aa
Joey Zielinski <joeyzielinski10@gmail.com>
2020-01-12 18:11:02 -0500

    documentation and cors

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 const teamController = require('./controllers/teams');
 const itemController = require('./controllers/items');
 
 app.use(helmet());
 
+app.use(cors());
+
 app.use(bodyParser.json({
     type: 'application/json'
 }));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* Teams */
 app.get('/api/teams', teamController.getTeams);
