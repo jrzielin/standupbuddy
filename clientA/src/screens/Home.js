691c4cc1227ea2564105be5700ae3b293a4dd35e
Joey Zielinski <joeyzielinski10@gmail.com>
2020-01-12 15:44:23 -0500

    change folder name

---
+++ clientA/src/screens/Home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            teams: []
+        };
+    }
+
+    componentDidMount() {
+        this.getTeams();
+    }
+
+    getTeams = () => {
+        fetch('/api/teams')
+        .then(res => res.json())
+        .then(res => this.setState({teams: res.teams}));
+    }
+
+    // foo baz
+
+    render() {
+        return (
+            <section className="section">
+                <div className="container">
+                    <h1 className="title">Home Screen</h1>
+                    { this.state.teams.map(team => {
+                        return (
+                            <div>{team.name}</div>
+                        );
+                    }) }
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Home;
