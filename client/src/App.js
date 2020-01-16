5ca255f0f11c756df4e532d2db3b10fc76765acf
Joey Zielinski <joeyzielinski10@gmail.com>
2020-01-15 21:13:14 -0500

    404 screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import StandupList from './components/StandupList';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import UnAuthRoute from './components/UnAuthRoute';
 import HomeRoute from './components/HomeRoute';
 import PrivateRoute from './components/PrivateRoute';
+import NotFound from './screens/NotFound';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         <UnAuthRoute path="/login" component={Login} />
         <UnAuthRoute path="/signup" component={Signup} />
         <PrivateRoute path="/items" component={StandupList} />
-        <HomeRoute path="/" component={StandupList} />
+        <HomeRoute exact path="/" component={StandupList} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </Router>
   );
---
+++ client/src/screens/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NotFound(props) {
+    document.title = 'Standup Buddy | 404 Not Found';
+
+    return (
+        <section className="section">
+            <div className="container" style={{ textAlign: 'center' }}>
+                <h1 className="title">404 Not Found</h1>
+                <p className="subtitle">Could not find the page you are looking for.</p>
+                <p><Link to="/" className="button is-primary">Home</Link></p>
+            </div>
+        </section>
+    );
+}
